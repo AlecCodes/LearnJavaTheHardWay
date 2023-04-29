8923e06ae7683640009921d76f3f03b20bde0d7d
Alec Hannaford <alec322@gmail.com>
2023-04-29 16:21:59 -0700

    string fun functional

---
+++ ex5/StringFunFunctionalShort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringFunFunctionalShort {
+    public static void main( String[] args ){
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter a message: ");
+        System.out.println(camelcase(reverse(keyboard.nextLine())));
+    }
+
+    public static String reverse( String s ){
+        String rev = "";
+        for ( int i=s.length()-1; i>=0; i--)
+            rev += s.substring(i, i+1);
+
+        return rev;
+    }
+
+    public static String camelcase(String s){
+        String[] words = s.t,toLowerCase().split(" ");
+        String result = "";
+        for ( String w : words)
+            result += w.substring(0,1).toUpperCase() + w.substring(1);
+    return result;
+    }
+}
