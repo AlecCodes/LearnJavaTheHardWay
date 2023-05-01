c0dfb28044136a996210d0403cd5d9f21757943d
Alec Hannaford <alec322@gmail.com>
2023-05-01 16:53:33 -0700

    ex7 done

--- ex7/SquareRootFinder.java
+++ ex7/SquareRootFinder.java
@@ -1,4 +1,19 @@
 public class SquareRootFinder {
     double n;
     int iterations;
+
+    public void setNumber( double number){
+        n = number;
+        iterations = 8;
+        if ( n < 7 )
+            iterations++;
+    }
+
+    public double getRoot(){
+        double x = n/4;
+        for ( int i = 0; i<iterations; i++){
+            x = (x+(n/x))/2.0;
+        }
+        return x;
+    }
 }
