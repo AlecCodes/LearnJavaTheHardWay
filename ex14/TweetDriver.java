0d79ee26000e8c0865f25342c59dbc6d942d08d7
Alec Hannaford <alec322@gmail.com>
2023-06-10 15:28:54 -0700

    ex15 halfway done

--- ex14/TweetDriver.java
+++ ex14/TweetDriver.java
@@ -1,6 +1,6 @@
 public class TweetDriver{
     public static void main( String[] args){
-        Tweet t = new Tweet( "Thu Feb 19 20:29:00 + 0000", 9, true, 568507566168223744L, 2, false, "You can now buy vbucks using bitcoin! probable!" + "\n Thnks!");
+        Tweet t = new Tweet( "Thu Feb 19 20:29:00 + 0000", 9, true, 568507566168223744l, 2, false, "You can now buy vbucks using bitcoin! probable!" + "\n Thnks!");
     System.out.println(t.toString());
     System.out.println("\n ------------ \n");
     System.out.println( t );
