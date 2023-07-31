22645ef225ec898acf7abb1b3a87776778a413bf
Alec Hannaford <alec322@gmail.com>
2023-07-30 18:43:56 -0700

    adding

--- ex16/ArrayListIntro.java
+++ ex16/ArrayListIntro.java
@@ -3,5 +3,17 @@ import java.util.ArrayList;
 public class ArrayListIntro{
     public static void main(String[] args){
         ArrayList<String> arr = new ArrayList<String>();
+        System.out.println( "ArrayList has size(): " + arr.size());
+
+        arr.add("Ant-man");
+        arr.add("Hulk");
+        arr.add("Thor");
+        arr.add("Iron Man");
+        arr.add("Wasp");
+
+        System.out.println( "ArrayList has size(): " + arr.size());
+        System.out.println("Current Contents:" + arr );
+        //showList(arr);
+
     }
 }
