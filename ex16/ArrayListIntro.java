1b2b67b54988f381bef08fbbc13889b95468bf9f
Alec Hannaford <alec322@gmail.com>
2023-08-01 16:27:35 -0700

    main method

--- ex16/ArrayListIntro.java
+++ ex16/ArrayListIntro.java
@@ -13,7 +13,37 @@ public class ArrayListIntro{
 
         System.out.println( "ArrayList has size(): " + arr.size());
         System.out.println("Current Contents:" + arr );
-        //showList(arr);
+        showList(arr);
+
+        arr.add(3, "Jarvis"); //inserts into slot 3
+        showList(arr);
+
+        int i = arr.indexOf("Hulk");
+        System.out.println(arr.get(i) + " is located in slot " + i);
+        arr.remove(i);
+        showList(arr);
+
+        arr.add(1, "Captain America");
+        showList(arr);
+
+        arr.set(1, "Cap"); //set replaces value in slot 1
+        arr.remove(5);
+        arr.remove(4);
+        arr.remove(2);
+        arr.remove(0);
+        showList(arr);
+
+        arr.add("Hawkeye");
+        arr.add("Quicksilver");
+        arr.add("Scarlet Witch");
+        showList(arr);
+
+        i = arr.indexOf("Hawkeye");
+        String removed = arr.remove(i)
+        arr.add(i-1, removed);
+        showList(arr);
+
+        System.out.println("Current contents: " + arr );
 
     }
 }
